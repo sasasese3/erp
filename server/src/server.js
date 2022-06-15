132925652c9ceb929d4319af76ba9d4cc8b2afc4
KengAcethylene <keng2120@gmail.com>
2022-06-15 23:03:14 +0700

    add session authentication

--- server/src/server.js
+++ server/src/server.js
@@ -1,24 +1,50 @@
 const express = require("express");
+const session = require('express-session');
 const cors = require("cors");
-const { sequelize } = require('./utils/sequelize');
 const router = require('./controllers/router');
+const { sequelize } = require('./utils/sequelize');
+const morgan = require('morgan');
+const passport = require('passport');
+
+//redis for session
+const RedisStore = require('connect-redis')(session);
+const { createClient } = require('redis');
+const redisClient = createClient({ url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`, legacyMode: true });
+redisClient.connect()
+  .then(console.log('Connect to Redis server'))
+  .catch(console.error);
 
 const app = express();
 
 
-//uplode
-
+//cors
 app.use(cors());
+//json
 app.use(express.json());
+//other body type
 app.use(express.urlencoded({ extended: false }));
-
+//logger
+app.use(morgan('dev'));
 //use express static folder
 app.use(
   express.static(__dirname + "./Signature")
 );
+//session
+app.use(
+  session({
+    store: new RedisStore({ client: redisClient }),
+    saveUninitialized: false,
+    secret: process.env.SESSION_SECRET || 'keyboard cat',
+    resave: false,
+    cookie: { maxAge: 3600, secure: false }
+  })
+);
+app.use(
+  passport.authenticate('session')
+);
 
 sequelize.sync({ alter: true })
-  .then(console.log('Connect to the MySQL server, All models were synchronized successfully.'))
+  .then(console.log('Connect to MySQL server, All models were synchronized successfully.'))
   .catch((error) => console.error('Unable to sync to the models:', error));
 
 app.use(router);
--- server/src/utils/connectDB.js
+++
@@ -1,7 +0,0 @@
-module.exports = require('mysql2').createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    port: 3306,
-    password: process.env.MYSQL_ROOT_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-});
