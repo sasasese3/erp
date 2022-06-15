1f80c136019ea58a2c06cf092731c1890a85cd41
KengAcethylene <keng2120@gmail.com>
2022-06-15 14:24:01 +0700

    refactor structure (IB)

--- server/src/controllers/erp/erp.js
+++ server/src/controllers/erp/erp.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const ap3Router = require('./ap3');
+const ibRouter = require('./ib');
 
 router.use('/ap3', ap3Router);
+router.use('/ib', ibRouter);
 
 module.exports = router;
