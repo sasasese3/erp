f76d55f518f1b101b5af9660adb83d760a85b3f9
KengAcethylene <keng2120@gmail.com>
2022-06-29 22:03:11 +0700

    add PV api for inspector

--- server/src/controllers/inspector.js
+++ server/src/controllers/inspector.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { body, validationResult, param } = require('express-validator');
-const { PO, RV } = require('../utils/sequelize');
+const { PO, RV, PV } = require('../utils/sequelize');
 const { pdfEditor } = require('../utils/pdfEditor');
 const { unlinkSync } = require('fs');
 
@@ -98,6 +98,56 @@ router.patch('/rv/:id', [
         await rv.save();
 
         return res.json({ msg: 'Update RV Success' });
+    } catch (error) {
+        console.log(error);
+        return res.status(400).json({ msg: "Something went wrong", error: error });
+    }
+});
+
+router.patch('/pv/:id', [
+    param('id').notEmpty().isInt(),
+    body('approved').notEmpty().isBoolean(),
+], async (req, res) => {
+    try {
+        //* check error
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ msg: "Invalid Body", error: errors.array() });
+        }
+
+        //* get param and body data
+        const id = parseInt(req.params.id);
+        const { approved } = req.body;
+
+        //* get pv data
+        const pv = await PV.findByPk(id);
+        if (!pv) {
+            return res.status(400).json({ msg: 'id is invalid' });
+        }
+
+        //* change it to js object
+        const date = new Date();
+        const pv_data = pv.toJSON();
+
+        if (!approved) {
+            //* update db
+            pv.status = 'rejected';
+            pv.file_path = "";
+            unlinkSync(pv_data.file_path);
+        }
+        else {
+            //* edit existing pdf
+            const newFilePath = await pdfEditor(pv_data.file_path, 'pv', date);
+            //* update db
+            pv.status = 'approved';
+            pv.file_path = newFilePath;
+        }
+        pv.inspectorId = req.user.id;
+
+        //* save to db
+        await pv.save();
+
+        return res.json({ msg: 'Update PV Success' });
     } catch (error) {
         console.log(error);
         return res.status(400).json({ msg: "Something went wrong", error: error });
