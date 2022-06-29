2ff36436f8f818212e73d8a8180a02962edfbf34
KengAcethylene <keng2120@gmail.com>
2022-06-29 22:11:51 +0700

    add PV router

--- server/src/controllers/erp/index.js
+++ server/src/controllers/erp/index.js
@@ -2,13 +2,13 @@ const router = require("express").Router();
 const poRouter = require('./po');
 // const ap3Router = require('./ap3');
 // const ibRouter = require('./ib');
-// const pvRouter = require('./pv');
+const pvRouter = require('./pv');
 const rvRouter = require('./rv');
 
 // router.use('/ap3', ap3Router);
 // router.use('/ib', ibRouter);
 router.use('/po', poRouter);
-// router.use('/pv', pvRouter);
+router.use('/pv', pvRouter);
 router.use('/rv', rvRouter);
 
 module.exports = router;
