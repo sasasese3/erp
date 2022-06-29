3e8292c35b8f3cfed2a9691fa2890f473d43788d
KengAcethylene <keng2120@gmail.com>
2022-06-29 15:44:34 +0700

    add RV model

--- server/src/controllers/erp/po.js
+++ server/src/controllers/erp/po.js
@@ -1,15 +1,16 @@
 const router = require('express').Router();
-const path = require('path');
+const { join } = require('path');
+const { body, validationResult, param } = require('express-validator');
+const { createWriteStream, readFileSync } = require('fs');
+const { UniqueConstraintError, } = require('sequelize');
+
 const { printer, pdfFolder } = require('../../utils/pdfPrinter');
 const { POPdfDeifinition } = require('../../utils/POPdfDefinition');
-const { body, validationResult, param } = require('express-validator');
-const fs = require('fs');
-const { PO, PO_Product, Product, Employee, Supplier, sequelize } = require('../../utils/sequelize');
-const { UniqueConstraintError, } = require('sequelize');
 const userRoles = require("../../utils/userRoles");
+const { PO, PO_Product, Product, Employee, Supplier, sequelize } = require('../../utils/sequelize');
 
 //pdf file path
-const poPDFFolder = path.join(pdfFolder, 'po');
+const poPDFFolder = join(pdfFolder, 'po');
 
 router.post('/', [
     body('SupplierId').notEmpty().isInt(),
@@ -23,19 +24,26 @@ router.post('/', [
     const t = await sequelize.transaction();
 
     try {
+
+        //* error input validation
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(422).json({ msg: "Invalid Body", error: errors.array() });
         }
+
+        //* get body data
         const { SupplierId, createdAt, total_price, products } = req.body;
 
+        //* create filepath
         const date = new Date();
-        const filePath = path.join(poPDFFolder, `${date.getTime()}.pdf`);
+        const filePath = join(poPDFFolder, `${date.getTime()}.pdf`);
 
+        //* create po
         const po = await PO.create({
             total_price, file_path: filePath, createdAt, EmployeeId: req.user.id, SupplierId
         }, { transaction: t });
 
+        //* create po-product
         await PO_Product.bulkCreate(
             products.map((product) => ({
                 ...product,
@@ -43,16 +51,21 @@ router.post('/', [
             })), { transaction: t }
         );
 
+        //* get query
         const data = await PO.findByPk(po.id, { include: [Product, Employee], order: [[Product, PO_Product, 'no', 'ASC']] });
+
+        //* create pdf
         const doc = printer.createPdfKitDocument(POPdfDeifinition(data.toJSON()));
-        doc.pipe(fs.createWriteStream(filePath));
+        doc.pipe(createWriteStream(filePath));
         doc.end();
 
+        //* commit transaction
         await t.commit();
 
         return res.json({ msg: 'Create PO Success' });
 
     } catch (error) {
+        //* revert transaction
         await t.rollback();
         if (error instanceof UniqueConstraintError) {
             return res.status(400).json({ msg: "กรุณาไม่เลือกสินค้าซ้ำ" });
@@ -97,7 +110,7 @@ router.get('/pdf/:id', [
         if (!po) {
             return res.status(403).json({ msg: 'Forbidden' });
         }
-        const data = fs.readFileSync(po.toJSON().file_path);
+        const data = readFileSync(po.toJSON().file_path);
         res.contentType("application/pdf");
         res.send(data);
 
@@ -105,201 +118,5 @@ router.get('/pdf/:id', [
         return res.status(400).json({ msg: "Something went wrong", error: error });
     }
 });
-// router.route('/')
-//     //SelectPO
-//     .get(function (req, res) {
-//         // Store hash in your password DB.
-//         connectDB.query("SELECT * FROM  po", [], function (err, POdata) {
-//             if (err) {
-//                 res.json({ status: "error", message: err });
-//                 return;
-//             }
-//             res.json({ status: "ok", POdata });
-//         });
-//     })
-//     //CreatePO
-//     .post(function (req, res) {
-//         // Store hash in your password DB.
-//         connectDB.query(
-//             "INSERT INTO erp_systems.po (PO_ID,SUPPLIER_ID,CUSTOMER_ID,EMPLOYEE_ID_CREATOR,PO_CREATOR,EMPLOYEE_ID_APPROVER,PO_APPROVER,PO_DETAIL,PO_AMOUNTPRODUCT,PO_DATE,PO_STATUS) VALUES (? ,? ,? ,?,?,?,?,?,?,?,?)",
-//             [
-//                 req.body.PO_ID,
-//                 req.body.SUPPLIER_ID,
-//                 req.body.CUSTOMER_ID,
-//                 req.body.EMPLOYEE_ID_CREATOR,
-//                 req.body.PO_CREATOR,
-//                 req.body.EMPLOYEE_ID_APPROVER,
-//                 req.body.PO_APPROVER,
-//                 req.body.PO_DETAIL,
-//                 req.body.PO_AMOUNTPRODUCT,
-//                 req.body.PO_DATE,
-//                 req.body.PO_STATUS,
-//             ],
 
-//             function (err) {
-//                 if (err) {
-//                     res.json({ status: "error", message: err });
-//                     return;
-//                 }
-//                 connectDB.query(
-//                     "SELECT * FROM product WHERE PRODUCT_ID =" +
-//                     ConvertToDetailProductQuery(req.body.PO_DETAIL), //Detailของแต่ละใบ
-//                     function (err, DataProduct) {
-//                         if (err) {
-//                             res.json({ status: "error", message: err });
-//                         }
-//                         //เอาราคาของแต่ละสินค้าออกมา
-//                         let PriceOneArray = [];
-//                         for (let i = 0; i < ConvertToArray(req.body.PO_DETAIL).length; i++) {
-//                             //Detailของแต่ละใบ
-//                             PriceOneArray.push(DataProduct[i].PRODUCT_PRICE);
-//                         }
-//                         //ราคารวมของแต่ละชิ้น
-//                         let AmountArray = ConvertToArray(req.body.PO_AMOUNTPRODUCT); //AMOUNTPRODUCT ของแต่ละใบ
-//                         let PriceArray = [];
-//                         for (let i = 0; i < AmountArray.length; i++) {
-//                             let Ans = PriceOneArray[i] * AmountArray[i];
-//                             PriceArray.push(Ans);
-//                         }
-//                         //ราคารวมทั้งหมด
-//                         let PriceTotal = 0;
-//                         for (let i = 0; i < PriceArray.length; i++) {
-//                             PriceTotal += PriceArray[i];
-//                         }
-//                         //เอาราคารวมเข้าไปเก็บ
-//                         connectDB.query(
-//                             "UPDATE erp_systems.po SET PO_PRICETOTAL = ? where PO_ID = ? ",
-//                             [PriceTotal, req.body.PO_ID], //ID ของใบนั้นๆ
-//                             function (err) {
-//                                 if (err) {
-//                                     res.json({
-//                                         status: "error insert PO_PRICETOTAL ",
-//                                         message: err,
-//                                     });
-//                                 } else {
-//                                     res.json({
-//                                         status: "ok",
-//                                         message: "INSERT PO success",
-//                                     });
-//                                 }
-//                             }
-//                         );
-//                     }
-//                 );
-//             }
-//         );
-//     })
-//     //UpdatePO
-//     .patch(function (req, res) {
-//         // Store hash in your password DB.
-//         connectDB.query(
-//             "UPDATE erp_systems.po SET SUPPLIER_ID = ? ,CUSTOMER_ID = ? ,EMPLOYEE_ID_CREATOR = ? ,PO_CREATOR = ? ,EMPLOYEE_ID_APPROVER = ? , PO_APPROVER = ? ,PO_DETAIL = ? ,PO_AMOUNTPRODUCT = ? ,PO_DATE = ? ,PO_STATUS = ?  where PO_ID  = ? ",
-//             [
-//                 req.body.SUPPLIER_ID,
-//                 req.body.CUSTOMER_ID,
-//                 req.body.EMPLOYEE_ID_CREATOR,
-//                 req.body.CREATOR,
-//                 req.body.EMPLOYEE_ID_APPROVER,
-//                 req.body.APPROVER,
-//                 req.body.PO_DETAIL,
-//                 req.body.PO_AMOUNTPRODUCT,
-//                 req.body.PO_DATE,
-//                 req.body.PO_STATUS,
-//                 req.body.PO_ID,
-//             ],
-
-//             function (err) {
-//                 if (err) {
-//                     res.json({ status: "error", message: err });
-//                     return;
-//                 }
-//                 connectDB.query(
-//                     "SELECT * FROM product WHERE PRODUCT_ID =" +
-//                     ConvertToDetailProductQuery(req.body.PO_DETAIL), //Detailของแต่ละใบ
-//                     function (err, DataProduct) {
-//                         if (err) {
-//                             res.json({ status: "error", message: err });
-//                         }
-//                         //เอาราคาของแต่ละสินค้าออกมา
-//                         let PriceOneArray = [];
-//                         for (let i = 0; i < ConvertToArray(req.body.PO_DETAIL).length; i++) {
-//                             //Detailของแต่ละใบ
-//                             PriceOneArray.push(DataProduct[i].PRODUCT_PRICE);
-//                         }
-//                         //ราคารวมของแต่ละชิ้น
-//                         let AmountArray = ConvertToArray(req.body.PO_AMOUNTPRODUCT); //AMOUNTPRODUCT ของแต่ละใบ
-//                         let PriceArray = [];
-//                         for (let i = 0; i < AmountArray.length; i++) {
-//                             let Ans = PriceOneArray[i] * AmountArray[i];
-//                             PriceArray.push(Ans);
-//                         }
-//                         //ราคารวมทั้งหมด
-//                         let PriceTotal = 0;
-//                         for (let i = 0; i < PriceArray.length; i++) {
-//                             PriceTotal += PriceArray[i];
-//                         }
-//                         //เอาราคารวมเข้าไปเก็บ
-//                         connectDB.query(
-//                             "UPDATE erp_systems.po SET PO_PRICETOTAL = ? where PO_ID = ? ",
-//                             [PriceTotal, req.body.PO_ID], //ID ของใบนั้นๆ
-//                             function (err) {
-//                                 if (err) {
-//                                     res.json({
-//                                         status: "error insert PO_PRICETOTAL ",
-//                                         message: err,
-//                                     });
-//                                 } else {
-//                                     res.json({
-//                                         status: "ok",
-//                                         message: "Update PO success",
-//                                     });
-//                                 }
-//                             }
-//                         );
-//                     }
-//                 );
-//             }
-//         );
-//     });
-
-//DeletePO
-// router.delete('/:PO_ID', function (req, res) {
-//     connectDB.execute(
-//         "DELETE FROM erp_systems.po WHERE PO_ID  = ?",
-//         //ส่ง EMPLOYEE_ID มากับ Path
-//         [req.params.PO_ID],
-
-//         function (err) {
-//             if (err) {
-//                 res.json({ status: "error", message: err });
-//                 return;
-//             } else {
-//                 res.json({ status: "ok", message: "Delete success" });
-//             }
-//         }
-//     );
-// });
-
-//เรียกดูใบสำคัญที่ยังไม่ได้!!อนุมัติ
-// router.get("/SelectNonApprovePO", function (req, res) {
-//     // Store hash in your password DB.
-//     connectDB.query("SELECT * FROM  po Where PO_STATUS = 0 ", [], function (err, POdata) {
-//         if (err) {
-//             res.json({ status: "error", message: err });
-//             return;
-//         }
-//         res.json({ status: "ok", POdata });
-//     });
-// });
-
-//อัพโหดไฟล์รูปลายเซ็นต์
-// router.post("/uploadPO", uploadPO.single("fileupload"), (req, res) => {
-//     let Filename = "file-" + "-" + req.file.originalname.split(".")[0] + new Date().getFullYear() + "-" + (new Date().getMonth() + 1) + "-" + new Date().getDate() + "-Time-" + new Date().getHours() + "-" + new Date().getMinutes() + "." + req.file.originalname.split(".")[req.file.originalname.split(".").length - 1];
-//     var imgsrc = "D:/Project_จบ/Fullstack-Project/server/Signature/PO/" + Filename;
-//     var insertData = "UPDATE PO SET PO_PATHSIGNATURE = ? WHERE PO_ID = ? ";
-//     connectDB.query(insertData, [imgsrc, req.body.PO_ID], (err, result) => {
-//         if (err) throw err;
-//         console.log("file uploaded");
-//     });
-// });
 module.exports = router;
---
+++ server/src/models/rv.js
@@ -0,0 +1,53 @@
+module.exports = (sequelize, DataTypes) => {
+    const RV = sequelize.define('RV', {
+        rv_id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        total_price: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        status: {
+            type: DataTypes.ENUM,
+            values: ['pending', 'rejected', 'approved'],
+            defaultValue: 'pending'
+        },
+        file_path: DataTypes.STRING,
+        createdAt: {
+            type: "TIMESTAMP",
+            defaultValue: sequelize.literal("CURRENT_TIMESTAMP"),
+            allowNull: false,
+        },
+        id: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        customerName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        detail: DataTypes.STRING,
+    }, {
+        timestamps: true,
+    });
+
+    const RV_Product = sequelize.define('RV_Product', {
+        amount: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        no: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        price: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+    }, {
+        timestamps: false,
+    });
+    return { RV, RV_Product };
+};
