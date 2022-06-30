7c3babb08f26059cf0a11d305e4e9c66903e0bbe
KengAcethylene <keng2120@gmail.com>
2022-06-30 11:03:21 +0700

    fix create pdf & edit pdf method

--- server/src/controllers/inspector.js
+++ server/src/controllers/inspector.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { body, validationResult, param } = require('express-validator');
-const { PO, RV, PV } = require('../utils/sequelize');
-const { pdfEditor } = require('../utils/pdfEditor');
+const { PO, RV, PV, Supplier, Product, Employee, PO_Product, PV_Product, RV_Product } = require('../utils/sequelize');
+const { createPDF } = require('../utils/pdfPrinter');
 const { unlinkSync } = require('fs');
 
 
@@ -21,28 +21,28 @@ router.patch('/po/:id', [
         const { approved } = req.body;
 
         //* get po data
-        const po = await PO.findByPk(id);
+        const po = await PO.findByPk(id, { include: [Supplier, Product, Employee], order: [[Product, PO_Product, 'no', 'ASC']] });
         if (!po) {
             return res.status(400).json({ msg: 'id is invalid' });
         }
 
         //* change it to js object
-        const date = new Date();
         const po_data = po.toJSON();
 
         if (!approved) {
             //* update db
             po.status = 'rejected';
             po.file_path = "";
-            unlinkSync(po_data.file_path);
         }
         else {
-            //* edit existing pdf
-            const newFilePath = await pdfEditor(po_data.file_path, 'po', date);
+            //* create new pdf
+            const newFilePath = createPDF('po', po_data, false, null);
             //* update db
             po.status = 'approved';
             po.file_path = newFilePath;
         }
+
+        unlinkSync(po_data.file_path);
         po.inspectorId = req.user.id;
 
         //* save to db
@@ -70,28 +70,28 @@ router.patch('/rv/:id', [
         const { approved } = req.body;
 
         //* get rv data
-        const rv = await RV.findByPk(id);
+        const rv = await RV.findByPk(id, { include: [Supplier, Product, Employee], order: [[Product, RV_Product, 'no', 'ASC']] });
         if (!rv) {
             return res.status(400).json({ msg: 'id is invalid' });
         }
 
         //* change it to js object
-        const date = new Date();
         const rv_data = rv.toJSON();
 
         if (!approved) {
             //* update db
             rv.status = 'rejected';
             rv.file_path = "";
-            unlinkSync(rv_data.file_path);
         }
         else {
-            //* edit existing pdf
-            const newFilePath = await pdfEditor(rv_data.file_path, 'rv', date);
+            //* create new pdf 
+            const newFilePath = createPDF('rv', rv_data, false, null);
             //* update db
             rv.status = 'approved';
             rv.file_path = newFilePath;
         }
+
+        unlinkSync(rv_data.file_path);
         rv.inspectorId = req.user.id;
 
         //* save to db
@@ -120,28 +120,28 @@ router.patch('/pv/:id', [
         const { approved } = req.body;
 
         //* get pv data
-        const pv = await PV.findByPk(id);
+        const pv = await PV.findByPk(id, { include: [Supplier, Product, Employee], order: [[Product, PV_Product, 'no', 'ASC']] });
         if (!pv) {
             return res.status(400).json({ msg: 'id is invalid' });
         }
 
         //* change it to js object
-        const date = new Date();
         const pv_data = pv.toJSON();
 
         if (!approved) {
             //* update db
             pv.status = 'rejected';
             pv.file_path = "";
-            unlinkSync(pv_data.file_path);
         }
         else {
             //* edit existing pdf
-            const newFilePath = await pdfEditor(pv_data.file_path, 'pv', date);
+            const newFilePath = createPDF('pv', pv_data, false, null);
             //* update db
             pv.status = 'approved';
             pv.file_path = newFilePath;
         }
+
+        unlinkSync(pv_data.file_path);
         pv.inspectorId = req.user.id;
 
         //* save to db
